882ac849db2b11fde17773e26eaba196241d9448
GPMRtgarcia <144000854+MCZThomasGarcia@users.noreply.github.com>
2024-10-09 23:34:38 +0200

    Commentaire

--- core/domain/src/main/java/org/mathieu/domain/models/location/LocationPreview.kt
+++ core/domain/src/main/java/org/mathieu/domain/models/location/LocationPreview.kt
@@ -2,7 +2,14 @@ package org.mathieu.domain.models.location
 
 /**
  *
+ * DAO of Location entity
+ *
+ *@property id The unique identifier for the location.
+ *@property name The name of the location.
+ *@property type The type or category of the location.
+ *@property dimension The specific dimension or universe where this location exists.
  */
+
 data class LocationPreview(
     var id: Int,
     var name: String,
